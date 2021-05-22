5213ae48a66698661a99efdc6ec37008d8066748
gmtma <43379424+gmtma@users.noreply.github.com>
2021-05-22 09:15:13 -0500

    lastupdate

--- js/onClick.js
+++ js/onClick.js
@@ -3,10 +3,18 @@ function clickedButton(){
     alert('You Clicked Me')
 }
 
-function tipCalculator(a=10,b=90){
-    let result =a+b;
-    alert(result);
+function tipCalculator(){
+    //document.getElementById("output");
+    var a=parseInt(document.getElementById("subtotal").value);
+    var b=parseInt(document.getElementById("tip").value);
+   // var result=0;
+    //result=a+b;
+    var lab=document.getElementById("result");
+    lab.value=a+b;
+    
+   // alert(result);
 }
+//assignment one 
 function gogogo() {
     alert("Yay, it works!");
 }
