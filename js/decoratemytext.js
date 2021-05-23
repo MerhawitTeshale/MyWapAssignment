7fa02cab43d040a093a5b16e7058f7d494119d5f
gmtma <43379424+gmtma@users.noreply.github.com>
2021-05-23 14:01:49 -0500

    D

--- js/decoratemytext.js
+++ js/decoratemytext.js
@@ -1,10 +1,23 @@
-"use strict"
 
-function biggerClick(){
-    document.getElementById("mytext").style.fontSize='24pt';
-    //word.style.fontSize="24pt";
-  //  alert("Hello world!");
+function pageLoad(){
+    var biggerBtn= document.getElementById("btn-big");
+    biggerBtn.onclick= biggerClick;
 }
+
+// function biggerClick(){
+//     document.getElementById("mytext").style.fontSize='24pt';
+//     //word.style.fontSize="24pt";
+//   //  alert("Hello world!");
+// }
 function onChange(){
     document.getElementById("mytext").style.fontWeight='bold';
+    document.getElementById("mytext").style.color='green';
+    document.getElementById("mytext").style.textUnderlinePosition='below';
 }
+window.onload= function(){
+    var biggerBtn=document.getElementById('btn-big');
+    biggerBtn.onclick= function(){
+        document.getElementById('mytext').style.fontSize='24pt';
+    };
+};
+
