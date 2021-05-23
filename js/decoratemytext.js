b0125bfef025e44d53f74e2a5ff825f3d72c419b
gmtma <43379424+gmtma@users.noreply.github.com>
2021-05-23 11:03:31 -0500

    today

---
+++ js/decoratemytext.js
@@ -0,0 +1,10 @@
+"use strict"
+
+function biggerClick(){
+    document.getElementById("mytext").style.fontSize='24pt';
+    //word.style.fontSize="24pt";
+  //  alert("Hello world!");
+}
+function onChange(){
+    document.getElementById("mytext").style.fontWeight='bold';
+}
